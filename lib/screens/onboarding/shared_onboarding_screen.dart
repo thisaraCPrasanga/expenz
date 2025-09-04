2936827b4877fb2183b6ce048d0725366d7cd092
Thisara C Prasanga <thisaraprasanga.dev@gmail.com>
2025-09-04 08:32:48 +0530

    Create Form and add validation for check is form null

--- lib/screens/onboarding/shared_onboarding_screen.dart
+++ lib/screens/onboarding/shared_onboarding_screen.dart
@@ -1,4 +1,6 @@
+import 'package:expenzapp/constants/colors.dart';
 import 'package:expenzapp/constants/constats.dart';
+import 'package:expenzapp/screens/user_data_screen.dart';
 import 'package:flutter/material.dart';
 
 class SharedOnboardingScreen extends StatelessWidget {
@@ -16,20 +18,56 @@ class SharedOnboardingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(kDefalutPadding),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-
+      child: Stack(
         children: [
-          Image.asset(imagePath, width: 250, fit: BoxFit.cover),
-          SizedBox(height: 20),
-          Text(
-            title,
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              Image.asset(imagePath, width: 250, fit: BoxFit.cover),
+              SizedBox(height: 20),
+              Text(
+                title,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 20),
+              Text(
+                description,
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+              ),
+            ],
           ),
-          SizedBox(height: 20),
-          Text(
-            description,
-            style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+          Positioned(
+            top: 0,
+            right: 0,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => UserDataScreen()),
+                  );
+                },
+                child: Container(
+                  height: 20,
+
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: kMainColor,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Center(
+                      child: Text(
+                        "Skip",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
--- lib/screens/user_data_screen.dart
+++ lib/screens/user_data_screen.dart
@@ -1,3 +1,5 @@
+import 'package:expenzapp/constants/colors.dart';
+import 'package:expenzapp/constants/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class UserDataScreen extends StatefulWidget {
@@ -8,8 +10,181 @@ class UserDataScreen extends StatefulWidget {
 }
 
 class _UserDataScreenState extends State<UserDataScreen> {
+  //for the check box
+  bool _rememberMe = false;
+
+  //form Key for the validation
+  final _formKey = GlobalKey<FormState>();
+
+  //controller for the text from feilds
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordcontroler = TextEditingController();
+  final TextEditingController _conformPasswordcontroler =
+      TextEditingController();
+
+  @override
+  void dispose() {
+    _userNameController.dispose();
+    _emailController.dispose();
+    _passwordcontroler.dispose();
+    _conformPasswordcontroler.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("User data")));
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Enter your \nPersonal Details",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(height: 10),
+
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 10),
+                      //User Name
+                      TextFormField(
+                        controller: _userNameController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please Enter your Name!";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Name",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          contentPadding: EdgeInsets.all(20),
+                        ),
+                      ),
+
+                      SizedBox(height: 10),
+                      //Email
+                      TextFormField(
+                        controller: _emailController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please Enter your Email!";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Email",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          contentPadding: EdgeInsets.all(20),
+                        ),
+                      ),
+
+                      SizedBox(height: 10),
+                      //Password
+                      TextFormField(
+                        controller: _passwordcontroler,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please Enter your Password!";
+                          }
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          contentPadding: EdgeInsets.all(20),
+                        ),
+                      ),
+
+                      SizedBox(height: 10),
+                      //Password
+                      TextFormField(
+                        controller: _conformPasswordcontroler,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please ReEnter your Password!";
+                          }
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Conform Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          contentPadding: EdgeInsets.all(20),
+                        ),
+                      ),
+
+                      SizedBox(height: 25),
+                      //Remember me for the next time
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Remember me for the next time",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: kGrey,
+                            ),
+                          ),
+                          Expanded(
+                            child: CheckboxListTile(
+                              activeColor: kMainColor,
+                              value: _rememberMe,
+                              onChanged: (value) {
+                                setState(() {
+                                  _rememberMe = value!;
+
+                                  print(_rememberMe);
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 50),
+                      GestureDetector(
+                        onTap: () {
+                          if (_formKey.currentState!.validate()) {
+                            //form is valid , process data
+                            String userName = _userNameController.text;
+                            String email = _emailController.text;
+                            String password = _passwordcontroler.text;
+                            String conformPassword =
+                                _conformPasswordcontroler.text;
+
+                            print(
+                              "$userName $email $password $conformPassword",
+                            );
+                          }
+                          ;
+                        },
+                        child: CustomButton(
+                          buttonName: "Submit",
+                          buttonColor: kMainColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
